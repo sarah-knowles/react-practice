a5d387b0b5e276981e282f5af1579f68a3ea548d
sarah-knowles <sjk339@gmail.com>
2021-03-08 19:58:39 +1300

    custom hook useFetch

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Bloglist from './Bloglist'
+import useFetch from './useFetch';
 
 const Homepage = () => {
 
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-useEffect(() => {
-  fetch('http://localhost:8000/blogs')
-  .then(res => {
-    return res.json()
-  })
-  .then(data => {
-    setBlogs(data);
-    setIsLoading(false);
-  });
-}, []);
+  const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className='home'>
+      { error && <div>{error}</div>}
       { isLoading && <div>Loading...</div>}
       {blogs && <Bloglist blogs={blogs} title='All Blogs' />}
       {/* <Bloglist blogs={blogs.filter((blog) => blog.author === 'Jane Austen')} title="Jane's Blogs"/> */}
